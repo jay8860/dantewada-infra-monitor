dc5eecbf05ccc407b89f7a579d7a52ebd2acc278
Jayant Nahata <jayantnahata@Jayants-MacBook-Air.local>
2025-12-15 15:05:53 +0530

    Expose X-Total-Count header for pagination

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["X-Total-Count"]
 )
 
 # Create logic for serving uploaded static files (photos)
